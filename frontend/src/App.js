990c876374f03fa4091075ad2137887a2d17467a
haider ali <haidertiger10@gmail.com>
2021-01-08 04:02:11 -0800

    initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Header from "./components/Header/header.component";
+import Footer from "./components/Footer/footer.component";
+
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <main>
+        <h1>Welcome TO ProShop!</h1>
+      </main>
+      <Footer />
+    </>
+  );
+};
+
+export default App;
---
+++ frontend/src/components/Footer/footer.component.js
@@ -0,0 +1,8 @@
+import React from "react";
+import './footer.styles.css'
+
+const Footer = () => {
+  return <footer>@ CopyRight ProShop</footer>;
+};
+
+export default Footer;
