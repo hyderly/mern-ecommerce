92a7fbd6731eb1d83d4fb76c0e1b6614893ba24e
haider ali <haidertiger10@gmail.com>
2021-02-11 17:10:32 -0800

    some fixes

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -19,8 +19,8 @@ const cartItemsFromStorage = localStorage.getItem("cartItems")
   ? JSON.parse(localStorage.getItem("cartItems"))
   : [];
 
-const userInfoFromStorage = localStorage.getItem("cartItems")
-  ? JSON.parse(localStorage.getItem("cartItems"))
+const userInfoFromStorage = localStorage.getItem("userInfo")
+  ? JSON.parse(localStorage.getItem("userInfo"))
   : null;
 
 const rootReducer = combineReducers({
