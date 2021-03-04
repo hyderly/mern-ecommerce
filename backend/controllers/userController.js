2bee205d108f1db62ad65767d050154edc396fdc
haider ali <haidertiger10@gmail.com>
2021-03-04 15:40:41 -0800

    some changes

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -143,7 +143,7 @@ export const deleteUser = asyncHandler(async (req, res) => {
 // @route GET /api/users/:id
 // @access Private/Admin
 export const getUserById = asyncHandler(async (req, res) => {
-  const user = await User.findById(req.params.id).select('-password');
+  const user = await User.findById(req.params.id).select("-password");
   if (user) {
     res.json(user);
   } else {
